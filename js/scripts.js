97f9a90b34c121ce1ffdfec5f15440a8c77cc7df
abkendal <kendal.adam@mail.com>
2015-07-24 14:34:23 -0400

    Add jQuery ui touch-punch

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,52 @@ $(document).ready(function(){
 
 	// When you click on the Ryan icon, show his parts
 	$(".ryan-icon").on("click", function(){
+		// $partLists.hide();
 		$(".ryan-parts").show();
 	});
 
 	// When you click on the Drew icon, show his parts
 	$(".drew-icon").on("click", function(){
+		// $partLists.hide();
 		$(".drew-parts").show();
 	});
 
 	// When you click on the Kristen icon, show her parts
 	$(".kristen-icon").on("click", function(){
+		// $partLists.hide();
 		$(".kristen-parts").show();
+	});
+
+	// When you click on the Christopher icon, show his parts
+	$(".christopher-icon").on("click", function(){
+		// $partLists.hide();
+		$(".christopher-parts").show();
+	});
+
+	// When you click on the hat icon, show the hat parts
+	$(".hat-icon").on("click", function(){
+		// $partLists.hide();
+		$(".hat-parts").show();
+	});
+
+
+
+
+
+
+
+
+
+
+	$(".icon").on("click", function(){
+		console.log("Test");
 	});
 
 
 	// Make all the parts draggable
-	$(".part").draggable();
+	$(".part").draggable().resizable({
+		autoHide: true
+	});
 
 	// Make reset button refresh the page
 	$("#reset").on("click", function(){
